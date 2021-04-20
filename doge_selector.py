c935a128dfcb9930148d141423b82d0219bd55e7
Trismeg <tony.physics@gmail.com>
2021-04-20 18:31:44 +0400

    Create doge_selector.py

---
+++ doge_selector.py
@@ -0,0 +1,51 @@
+from sys import argv
+import cryptos
+import time
+ 
+if len(argv)==2:
+	_, N = argv
+else:
+	print('Wrong number arguments. 1 expected. Give number of contestants')
+	exit()
+ 
+img='''
+░░░░░░░░░▄░░░░░░░░░░░░░░▄░░░░
+░░░░░░░░▌▒█░░░░░░░░░░░▄▀▒▌░░░
+░░░░░░░░▌▒▒█░░░░░░░░▄▀▒▒▒▐░░░
+░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐░░░
+░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐░░░
+░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌░░░ 
+░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌░░
+░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐░░
+░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌░
+░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌░
+▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐░
+▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐░
+░▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌░
+░▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐░░
+░░▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌░░
+░░░░▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀░░░
+░░░░░░▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀░░░░░
+░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▀▀░░░░░░░░'''
+ 
+print(img)
+ 
+number_contestants=int(N)
+doge=cryptos.Doge()
+current_block_height=doge.current_block_height()
+target_block_height=doge.current_block_height()+1
+ 
+print(f'WELCOME TO ALL {number_contestants} DOGGIES !!!!')
+print(f'CURRENT BLOCK HEIGHT IS {target_block_height-1}')
+print(f'WAITING FOR BLOCK {target_block_height}')
+ 
+while(current_block_height!=target_block_height):
+	time.sleep(5)
+	print('WOW')
+	current_block_height=doge.current_block_height()
+ 
+top_num=int(eval('0x'+doge.block_info(target_block_height)['hash']))
+winner=(top_num%number_contestants)+1
+print('--------------BOOYACASHA------------------')
+print(f'AND THE WINNER IS DOGGIE DOG #{winner} !!!!!')
